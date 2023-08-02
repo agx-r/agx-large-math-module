2ad360043eceb98217afe41c6a36df84039925af
AGX <55353943+agx-r@users.noreply.github.com>
2023-08-02 16:25:39 +0500

    Update vector_addition.py

--- agx-math-module/vector_addition.py
+++ agx-math-module/vector_addition.py
@@ -3,24 +3,25 @@ def add_vectors(vector1, vector2):
     Add two vectors in 2D or 3D space.
 
     Args:
-        vector1 (list): List representing the first vector.
-        vector2 (list): List representing the second vector.
+        vector1 (list): The first vector as a list of coordinates (x, y) or (x, y, z).
+        vector2 (list): The second vector as a list of coordinates (x, y) or (x, y, z).
 
     Returns:
-        list: Resulting vector after addition.
+        list: The sum of the two vectors as a list of coordinates.
     """
-    if len(vector1) != len(vector2):
-        raise ValueError("Vectors must have the same dimension for addition.")
-    
-    result_vector = [v1 + v2 for v1, v2 in zip(vector1, vector2)]
-    return result_vector
+    if len(vector1) == len(vector2):
+        result_vector = [v1 + v2 for v1, v2 in zip(vector1, vector2)]
+        return result_vector
+    else:
+        raise ValueError("Vectors must have the same number of dimensions (2D or 3D).")
 
 # Test the function
-vector_2d = [3, 7]
-vector_3d = [1, 4, 2]
-another_vector_2d = [0, -2]
-result_2d = add_vectors(vector_2d, another_vector_2d)
-result_3d = add_vectors(vector_3d, another_vector_2d)
+vector_2d = [3, 5]
+vector_3d = [1, 2, 4]
+another_vector_2d = [2, -1]
 
-print("Result (2D):", result_2d)
-print("Result (3D):", result_3d)
+sum_vector_2d = add_vectors(vector_2d, another_vector_2d)
+sum_vector_3d = add_vectors(vector_3d, another_vector_2d)
+
+print("Sum of 2D vectors:", sum_vector_2d)  # Output: [5, 4]
+print("Sum of 3D vectors:", sum_vector_3d)  # Output: [3, 1, 3]
