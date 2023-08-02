fffee16c58346f88008168379c44cd3ef6af925d
AGX <55353943+agx-r@users.noreply.github.com>
2023-08-02 16:23:23 +0500

    Update __init__.py

--- agx-math-module/__init__.py
+++ agx-math-module/__init__.py
@@ -1,10 +1,10 @@
 # __init__.py
 
-from .vector_addition import add_vectors
-from .vector_cross_product import cross_product
-from .vector_dot_product import dot_product
-from .vector_substraction import subtract_vectors
-from .magnitude_calculation import vector_magnitude
+from vector_addition import add_vectors
+from vector_cross_product import cross_product
+from vector_dot_product import dot_product
+from vector_substraction import subtract_vectors
+from magnitude_calculation import vector_magnitude
 
 from matrix_multiplication import matrix_multiplication
 from matrix_transposition import transpose_matrix
