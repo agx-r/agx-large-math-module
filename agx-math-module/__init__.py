2a780578c4d096147855c5088487d1bd95008848
AGX <55353943+agx-r@users.noreply.github.com>
2023-08-02 16:13:31 +0500

    Update __init__.py

--- agx-math-module/__init__.py
+++ agx-math-module/__init__.py
@@ -1,2 +1,5 @@
 # __init__.py
 from .vector_addition import add_vectors
+from .vector_cross_product import cross_product
+from .vector_dot_product import dot_product
+from .vector_substraction import subtract_vectors
